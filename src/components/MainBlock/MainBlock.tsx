e6308f6a186415d45480bda2c93c941c7b0a015f
Hanna <hanna.horbova@gmail.com>
2024-06-05 19:47:57 +0300

    refactoring of table component

--- src/components/MainBlock/MainBlock.tsx
+++ src/components/MainBlock/MainBlock.tsx
@@ -3,16 +3,103 @@ import TransactionTable from "../TransactionTable/TransactionTable";
 import styled from "styled-components";
 import FilterMenu from "../FilterMenu/FilterMenu";
 import ImportExportMenu from "../ImportExportMenu/ImportExportMenu";
+import PaginationMenu from "../PaginationMenu/PaginationMenu";
+import EditModal from "../EditModal/EditModal";
+import DeleteModal from "../DeletModal/DeleteModal";
+import {useState} from "react";
+import {Transaction} from "../../types/Transaction";
+import {useTDispatch, useTSelector} from "../hooks/reduxHooks";
+import {deleteTransactions, updateTransactionStatus} from "../../redux/transactionsSlice";
 
 
 const MainBlock = () => {
+    const [isDeleteModalOpen, setDeleteModalOpen] = useState<boolean>(false);
+    const [isEditModalOpen, setEditModalOpen] = useState<boolean>(false);
+    const [selectedTransaction, setSelectedTransaction] = useState<Transaction | null>(null);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const transactionsInPage = 10;
+
+    const dispatch = useTDispatch();
+    const transactions = useTSelector((state) => state.transactions.filteredTransactions);
+
+    const paginatedTransactions = transactions.slice((currentPage - 1) * transactionsInPage, currentPage *
+        transactionsInPage);
+
+    console.log(transactions);
+
+    const editBtnStyle = {
+        width: "80px",
+    };
+
+    //Deleting logic
+    const openDeleteModal = (transaction: Transaction) => {
+        setSelectedTransaction(transaction);
+        setDeleteModalOpen(true);
+        console.log("open DeleteModal");
+    };
+
+    const closeDeleteModal = () => {
+        console.log("close DeleteModal");
+        setDeleteModalOpen(false);
+        setSelectedTransaction(null);
+    };
+
+    const handleDeleteConfirm = () => {
+        if (selectedTransaction) {
+            dispatch(deleteTransactions(selectedTransaction.TransactionId));
+            closeDeleteModal();
+            console.log("Deleted ID " + selectedTransaction.TransactionId);
+        }
+    };
+
+
+    //Editing logic
+    const openEditModal = (transaction: Transaction) => {
+        setSelectedTransaction(transaction);
+        setEditModalOpen(true);
+        console.log("open EditModal");
+    };
+
+    const closeEditModal = () => {
+        console.log("close closeEditModal");
+        setEditModalOpen(false);
+        setSelectedTransaction(null);
+    };
+
+    const handleEditSubmit = (data: {   Status: string }) => {
+        if (selectedTransaction) {
+            dispatch(updateTransactionStatus({TransactionId: selectedTransaction.TransactionId, Status: data.Status}));
+
+        }
+    };
   return (
     <StyledMain>
       <Box display="flex" gap='100px' width="100%" justifyContent='space-between' marginBottom="10px">
         <FilterMenu />
         <ImportExportMenu />
       </Box>
-      <TransactionTable />
+      <TransactionTable paginatedTransactions={paginatedTransactions} onEdit={openEditModal} onDelete={openDeleteModal} />
+
+        <PaginationMenu transactions={transactions} currentPage={currentPage} onPageChange={setCurrentPage}
+                        transactionsInPage={transactionsInPage}/>
+
+        {selectedTransaction && (
+            <EditModal
+                isOpen={isEditModalOpen}
+                onClose={closeEditModal}
+                onSubmit={handleEditSubmit}
+                transaction={selectedTransaction}
+            />
+        )}
+
+        {selectedTransaction && (
+            <DeleteModal
+                isOpen={isDeleteModalOpen}
+                onClose={closeDeleteModal}
+                onConfirm={handleDeleteConfirm}
+                transaction={selectedTransaction}
+            />
+        )}
     </StyledMain>
   );
 };
--- src/components/TransactionTable/TransactionTable.tsx
+++ src/components/TransactionTable/TransactionTable.tsx
@@ -1,77 +1,21 @@
 import {Box, Table, Tbody, Td, Th, Thead, Tr} from "@chakra-ui/react";
 import CustomButton from "../commonUI/CustomButton/CustomButton";
-import {useState} from "react";
-import EditModal from "../EditModal/EditModal";
-import DeleteModal from "../DeletModal/DeleteModal";
-import {useTDispatch, useTSelector} from "../hooks/reduxHooks";
 import {Transaction} from "../../types/Transaction";
-import PaginationMenu from "../PaginationMenu/PaginationMenu";
-import {deleteTransactions, updateTransactionStatus} from "../../redux/transactionsSlice";
 
 
-const TransactionTable: React.FC = () => {
-    const [isDeleteModalOpen, setDeleteModalOpen] = useState<boolean>(false);
-    const [isEditModalOpen, setEditModalOpen] = useState<boolean>(false);
-    const [selectedTransaction, setSelectedTransaction] = useState<Transaction | null>(null);
-    const [currentPage, setCurrentPage] = useState<number>(1);
+interface TransactionTableProps {
+    paginatedTransactions: Transaction[];
+    onEdit: (transaction: Transaction) => void;
+    onDelete: (transaction: Transaction) => void;
+}
 
-    const dispatch = useTDispatch();
-    const transactions = useTSelector((state) => state.transactions.filteredTransactions);
-    const transactionsInPage = 10;
-    console.log(currentPage);
-    const paginatedTransactions = transactions.slice((currentPage - 1) * transactionsInPage, currentPage *
-        transactionsInPage);
-
-    console.log(transactions);
+const TransactionTable: React.FC<TransactionTableProps> = ({paginatedTransactions, onEdit, onDelete}) => {
 
     const editBtnStyle = {
         width: "80px",
     };
 
-    //Deleting logic
-    const openDeleteModal = (transaction: Transaction) => {
-        setSelectedTransaction(transaction);
-        setDeleteModalOpen(true);
-        console.log("open DeleteModal");
-    };
-
-    const closeDeleteModal = () => {
-        console.log("close DeleteModal");
-        setDeleteModalOpen(false);
-        setSelectedTransaction(null);
-    };
-
-    const handleDeleteConfirm = () => {
-       if (selectedTransaction) {
-                    dispatch(deleteTransactions(selectedTransaction.TransactionId));
-                    closeDeleteModal();
-                    console.log("Deleted ID " + selectedTransaction.TransactionId);
-                }
-    };
-
-
-    //Editing logic
-    const openEditModal = (transaction: Transaction) => {
-        setSelectedTransaction(transaction);
-        setEditModalOpen(true);
-        console.log("open EditModal");
-    };
-
-    const closeEditModal = () => {
-        console.log("close closeEditModal");
-        setEditModalOpen(false);
-        setSelectedTransaction(null);
-    };
-
-    const handleEditSubmit = (data: {   Status: string }) => {
-        if (selectedTransaction) {
-            dispatch(updateTransactionStatus({TransactionId: selectedTransaction.TransactionId, Status: data.Status}));
-
-        }
-    };
-
     return (
-
         <>
 
             <Box     minHeight='560px'>
@@ -96,9 +40,9 @@ const TransactionTable: React.FC = () => {
                                 <Td textAlign="center">{transaction.Amount}</Td>
                                 <Td display='flex' justifyContent='space-around'>
                                     <Box display='flex' width="170px" gap='10px' justifyContent='space-between'>
-                                        <CustomButton onClick={() => openEditModal(transaction)} btnName='Edit' type='button'
+                                        <CustomButton onClick={() => onEdit(transaction)} btnName='Edit' type='button'
                                                       customStyles={editBtnStyle}/>
-                                        <CustomButton onClick={() => openDeleteModal(transaction)} btnName='Delete'
+                                        <CustomButton onClick={() => onDelete(transaction)} btnName='Delete'
                                                       type='button'/>
                                     </Box>
                                 </Td>
@@ -112,7 +56,7 @@ const TransactionTable: React.FC = () => {
             </Box>
 
 
-            <PaginationMenu transactions={transactions} currentPage={currentPage} onPageChange={setCurrentPage}
+          {/*  <PaginationMenu transactions={transactions} currentPage={currentPage} onPageChange={setCurrentPage}
                             transactionsInPage={transactionsInPage}/>
 
             {selectedTransaction && (
@@ -131,7 +75,7 @@ const TransactionTable: React.FC = () => {
                     onConfirm={handleDeleteConfirm}
                     transaction={selectedTransaction}
                 />
-            )}
+            )}*/}
         </>
     );
 };
